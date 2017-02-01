7f77afdf477fb6a3b77e5459889bafe382ab4deb
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-02-01 00:16:47 -0500

    Update update.php

--- views/admin/update.php
+++ views/admin/update.php
@@ -1,8 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm
-use yii\widgets\DetailView;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
