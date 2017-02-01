058bdfd79a0f0112cdff3716ac57ce8df3c0375b
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-01-31 23:48:30 -0500

    Update _form.php

--- views/admin/_form.php
+++ views/admin/_form.php
@@ -19,7 +19,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'active')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?php echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
