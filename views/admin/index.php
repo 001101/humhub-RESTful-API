a9579e800f96c55dea073275773e6e4694de081b
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-08-28 10:08:13 -0700

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -10,7 +10,7 @@ use humhub\widgets\GridView;
 ?>
 
 <div class="panel panel-default">
-    <div class="panel-heading"><?php echo Yii::t('Restful_apiModule.base', '<strong>Manage</strong> Api Users'; ?></div>
+    <div class="panel-heading"><?= Yii::t('base', '<strong>Manage</strong> Api Users'); ?></div>
     <div class="panel-body">
     <?php echo \humhub\modules\api\widgets\ApiUserMenu::widget(); ?>
     <p />
