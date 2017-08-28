daac6fb9bb0473087a62dec049eac95cabd785a9
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-08-28 10:23:23 -0700

    Update m160309_181254_create_api_user.php

--- migrations/m160309_181254_create_api_user.php
+++ migrations/m160309_181254_create_api_user.php
@@ -7,12 +7,12 @@ class m160309_181254_create_api_user extends Migration
 
     public function up()
     {
-         $this->createTable('api_user', array(
+         $this->createTable('api_user', [
              'id' => 'pk',
              'client' => 'varchar(255) NOT NULL',
              'api_key' => 'varchar(32) NOT NULL',
              'active' => 'integer(1) NOT NULL',
-            ),
+            ],
         '');
     }
 
